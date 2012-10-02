145ed35fe7b491b10bc76351550d12d0fd1d081d
Mark Burns <markthedeveloper@gmail.com>
2012-10-02 14:28:39 +0100

    bumped version moved version number into markov_uuid.rb top level file

--- lib/markov_uuid.rb
+++ lib/markov_uuid.rb
@@ -1,9 +1,8 @@
-require_relative "./markov_uuid/version"
 require_relative './markov_uuid/key_selector'
 require_relative './markov_uuid/storage'
 require_relative './markov_uuid/markov'
 
 
 module MarkovUuid
-  # Your code goes here...
+  VERSION = "0.0.2"
 end
